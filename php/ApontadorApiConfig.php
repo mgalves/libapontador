2d0e080e3239030f8b6d231a0c1d42caeb7de6d6
Chester (Carlos Duarte do Nascimento <cd@pobox.com>
2010-10-04 15:52:24 -0300

    Inclusao da biblioteca php

---
+++ php/ApontadorApiConfig.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * ApontadorApiConfig.php
+ *
+ * Configurações do aplicativo que vai acessar a Apontador API via ApontadorApiLib.php
+ * (podem ser obtidas no perfil do usuário "dono" do aplicativo em http://www.apontador.com.br)
+ *
+ * Copyright 2010 Carlos Duarte do Nascimento (Chester)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http: *www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+ 
+function apontadorGetConfig() {
+	return array(
+		"email" => "",
+		"key" => "",
+		"secret" => "",
+		"callbackurl" =>  ""
+	);
+}
+
+$apontadorConfigTeste = apontadorGetConfig();
+if (!$apontadorConfigTeste["email"]) {
+	die("Por favor, cadastre sua aplicação no apontador e configure o ApontadorApiConfig.php");
+}
